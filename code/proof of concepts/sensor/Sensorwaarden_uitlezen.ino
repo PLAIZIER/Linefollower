fc5d07904ba75be310918e9f45ff672e7ef64c58
PLAIZIER <120053037+PLAIZIER@users.noreply.github.com>
2025-09-04 11:34:26 +0200

    Add files via upload

---
+++ code/proof of concepts/sensor/Sensorwaarden_uitlezen.ino
@@ -0,0 +1,40 @@
+// Pinnen toewijzing voor de QTR-8A sensoren
+//const int sensorPins[] = {34, 35, 32, 33, 25, 26, 27, 14}; 
+const int sensorPins[] = {14, 27, 26, 25, 33, 32, 35, 34}; 
+const int numSensors = 8; // Aantal sensoren
+
+void setup() {
+  // Initialiseer de seriële monitor
+  Serial.begin(115200);
+  
+  // Stel alle sensorpinnen in als invoer
+  for (int i = 0; i < numSensors; i++) {
+    pinMode(sensorPins[i], INPUT);
+  }
+
+  Serial.println("QTR-8A Sensor Waarden (elke seconde):");
+}
+
+void loop() {
+  // Array om sensorwaarden op te slaan
+  int sensorValues[numSensors];
+
+  // Lees de waarden van alle sensoren
+  for (int i = 0; i < numSensors; i++) {
+    sensorValues[i] = analogRead(sensorPins[i]);
+  }
+
+  // Print de waarden naar de seriële monitor
+  Serial.print("Sensor waarden: ");
+  for (int i = 0; i < numSensors; i++) {
+    Serial.print("D");
+    //Serial.print(sensorPins[i]);
+    Serial.print(": ");
+    Serial.print(sensorValues[i]);
+    if (i < numSensors - 1) Serial.print(", ");
+  }
+  Serial.println();
+
+  // Wacht 1 seconde
+  delay(20);
+}
